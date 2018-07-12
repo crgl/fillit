d6fc919b5d00a57e17b2950eeb04f0ebc67fa613
Charles Gleason <cgleason@student.42.us.org>
2018-07-12 15:19:05 -0700

    Add complete parsing pre-testing

--- dr_fill.c
+++ dr_fill.c
@@ -18,35 +18,140 @@ void	display_usage(void)
 	ft_putendl("usage: ./fillit tetrimino_list.fillit");
 }
 
-t_piece	parse_piece(int fd)
+t_piece	*which_piece(t_point *hashes)
 {
-	return (g_piece[NA]);
+	size_t	i;
+	size_t	j;
+	t_byte	xmin;
+	t_byte	ymin;
+
+	i = 0;
+	j = 0;
+	xmin = 4;
+	ymin = 4;
+	while (i < 4)
+	{
+		if (hashes[i].x < xmin)
+			xmin = hashes[i].x;
+		if (hashes[i].y < ymin)
+			ymin = hashes[i].y;
+		i++;
+	}
+	i = 0;
+	while (i < 4)
+	{
+		hashes[i].x -= xmin;
+		hashes[i].y -= ymin;
+		i++;
+	}
+	j = 0;
+	while (j < END)
+	{
+		i = 0;
+		while (i < 4 && hashes[i].x == g_piece[j].b[i].x &&
+				hashes[i].y == g_piece[j].b[i].y)
+			i++;
+		if (i == 4)
+			return (&g_piece[j]);
+		j++;
+	}
+	return (NULL);
+}
+
+t_piece	*parse_piece(char *square)
+{
+	t_piece	couldbe;
+	size_t	i;
+	t_byte	j;
+	t_point	hashes[4]
+
+	i = 0;
+	j = 0;
+	while (i < 20)
+	{
+		if (square[i] == '#' && (i + 1) % 5 != 0)
+		{
+			if (j < 4)
+				hashes[j++] = {i % 5, i / 5}
+			else
+				return (NULL);
+		}
+		else if (square[i] != '\n' && (i + 1) % 5 == 0)
+			return (NULL);
+		else if (square[i] != '.' && (i + 1) % 5 != 0)
+			return (NULL);
+		i++;
+	}
+	if (j < 4)
+		return (NULL);
+	return (which_piece(hashes));
+}
+
+t_piece	*parse_square(int fd)
+{
+	char	square[20]
+	char	c[1];
+	int		check;
+
+	check = read(fd, square, 20)
+	if (check == 20)
+	{
+		check = read(fd, c, 1);
+		if (c == '\n' || check = 0)
+		{
+			return (parse_piece(square));
+		}
+		return (NULL);
+	}
+	else if (check == 0)
+		return (&g_piece[END]);
+	else
+		return (NULL);
+}
+
+t_bool	death(t_piece *p1, t_piece *p2)
+{
+	int	i;
+
+	i = -1;
+	if (p1->width == p2->width && p1->height == p2->height)
+	{
+		while (++i < 4)
+			if (p1->b[i] != p2->b[i])
+				return (FALSE);
+		return (TRUE);
+	}
+	return (FALSE);
 }
 
 t_piece	*filet(int fd)
 {
-	t_piece	temp[26];
+	t_piece	temp[27];
 	size_t	i;
 	t_piece	*output;
+	t_piece	*input;
 
 	i = 0;
-	while (input = parse_piece(fd))
-		temp[i++] = input;
-	if (i == 0)
-		return (NULL)
-	output = (t_piece *)malloc((i + 1) * sizeof(t_piece));
-	output[i] = g_piece[NA];
+	input = &g_piece[I];
+	while (!(death(&input, &g_piece[END])))
+	{
+		if (i == 26)
+			return (NULL);
+		input = parse_square(fd);
+		if (input == NULL)
+			return (NULL);
+		temp[i++] = *input;
+	}
+	temp[i] = g_piece[END];
+	if (i < 2)
+		return (NULL);
+	output = (t_piece *)malloc(i * sizeof(t_piece));
+	output[--i] = g_piece[END];
 	while   (i)
 	{
 		i--;
-		if (!(temp[i]))
-		{
-			free(output);
-			return (NULL);
-		}
 		output[i] = temp[i];
 	}
-	
 	return (output);
 }
 
